e761dc3c049ba5eb70beecc0c51a86ef4c083f16
rihapfirdaus <muhammadrihap448@gmail.com>
2024-06-23 11:07:36 +0700

    feat: add spinner, add video preview and the difference

--- app/video_compressor/page.js
+++ app/video_compressor/page.js
@@ -1,11 +1,15 @@
 "use client";
-import { ArrowLeft, FileVideo, Home, RotateCcw } from "lucide-react";
+import { ArrowLeft, FileVideo, Home, RotateCcw, X } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function VideoCompressor() {
+  const [spinner, setSpinner] = useState(false);
+
   const [file, setFile] = useState(null);
   const [algorithm, setAlgorithm] = useState("");
+  const [preview, setPreview] = useState("");
+  const [showVideo, setShowVideo] = useState(false);
   const [response, setResponse] = useState({
     type: "",
     algorithm: "",
@@ -19,6 +23,12 @@ export default function VideoCompressor() {
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setPreview(e.target.result);
+    };
+    reader.readAsDataURL(event.target.files[0]);
   };
 
   const handleAlgorithmChange = (event) => {
@@ -36,6 +46,7 @@ export default function VideoCompressor() {
     const formData = new FormData();
     formData.append("file", file);
 
+    setSpinner(true);
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/video/compress?algorithm=${algorithm}`,
@@ -52,13 +63,61 @@ export default function VideoCompressor() {
       const result = await response.json();
 
       setResponse(result.data);
+      setSpinner(false);
     } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
+      setSpinner(false);
+      alert(`An error occurred: ${error.message}. Please tell the developer.`);
     }
   };
 
   return (
     <main className="box-border grid place-items-center w-screen h-screen">
+      {spinner && (
+        <div className="fixed top-0 bottom-0 left-0 right-0 backdrop-blur-md w-full h-full grid place-items-center z-10">
+          <img src="/loading.svg" className="size-20" />
+        </div>
+      )}
+
+      {showVideo && (
+        <div
+          className="fixed top-0 bottom-0 left-0 right-0 backdrop-blur-md w-full h-full grid place-items-center z-10"
+          onClick={() => setShowVideo(false)}
+        >
+          <button
+            className="text-bold hover:bg-blue-500 p-2 rounded-full hover:text-white absolute top-0 right-0 m-4"
+            onClick={() => setShowVideo(false)}
+          >
+            <X />
+          </button>
+          <div
+            className="flex gap-4 flex-wrap justify-center items-center p-4 w-full"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex flex-col gap-2 justify-center items-center">
+              <p>{response.url ? "before:" : "preview:"}</p>
+              <video
+                src={preview}
+                className="max-w-96"
+                alt={response.url ? "video before" : "video preview"}
+                controls
+                autoPlay={response.url ? false : true}
+              />
+            </div>
+            {response.url && (
+              <div className="flex flex-col gap-2 justify-center items-center">
+                <p>after:</p>
+                <video
+                  src={response.url}
+                  className="max-w-96"
+                  alt="video after"
+                  controls
+                  autoPlay
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
       <div className="flex flex-col gap-8 justify-center items-center bg-glass shadow-2xl py-8 px-8 md:py-16 md:px-24">
         <FileVideo className="size-16" />
         <h1 className="text-3xl">Video Compressor</h1>
@@ -89,6 +148,12 @@ export default function VideoCompressor() {
             >
               Compressed
             </a>
+            <button
+              className="hover:underline"
+              onClick={() => setShowVideo(true)}
+            >
+              see the difference
+            </button>
 
             <div className="w-full flex justify-center items-center p-2 gap-2">
               <Link
@@ -126,6 +191,19 @@ export default function VideoCompressor() {
                 accept="video/*"
                 onChange={handleFileChange}
               />
+
+              <button
+                type="button"
+                className={`${
+                  preview ? "visible" : "invisible"
+                } hover:underline cursor-pointer`}
+                onClick={() => {
+                  setShowVideo(true);
+                }}
+              >
+                preview
+              </button>
+
               <p>Select Algorithm:</p>
               <div className="flex w-fit">
                 <label className="flex gap-2 justify-center items-center py-2 px-4">
