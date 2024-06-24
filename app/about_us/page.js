b98f157b30bcd8b99783d3d79671f9d6d8ba46b4
rihapfirdaus <muhammadrihap448@gmail.com>
2024-06-24 09:48:08 +0700

    feat: add home button

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,7 +1,16 @@
+import { Home } from "lucide-react";
+import Link from "next/link";
+
 export default function AboutUs() {
   return (
     <main className="box-border grid place-items-center w-screen h-screen">
       <div className="flex flex-col gap-4 justify-center items-center bg-glass shadow-2xl py-8 px-4 md:py-16 md:px-24">
+        <Link
+          href="/"
+          className="text-bold hover:bg-blue-500 p-2 rounded-full hover:text-white"
+        >
+          <Home />
+        </Link>
         <h1 className="text-2xl underline">Our Team</h1>
         <p>Muhammad Raihan Firdaus - 1217050101</p>
         <p>Muhammad Rihap Firdaus - 1217050102</p>
